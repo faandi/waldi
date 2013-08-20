b005d667c9eef488e7f4f5586aee10b5b47c06cd
Andreas Fachathaler <faandi@fachi.net>
2013-08-20 22:20:19 +0000

    Add Template CompileFile and tests

--- Waldi.Lib/Engine/Template.cs
+++ Waldi.Lib/Engine/Template.cs
@@ -12,52 +12,77 @@ namespace Waldi.Engine
 {
     public class Template
     {
-		public static string CompileTemplate(string templatestring, IPackage package)
-		{
-			if (package == null) {
-				throw new ArgumentNullException ("package");
-			}
-			if (string.IsNullOrEmpty (templatestring)) {
-				throw new ArgumentException ("templatestring cannot be null or empty.", "templatestring");
-			}
-			string result = Razor.Parse(templatestring, package);
-			return result;
-		}
+		//public static string CompileTemplate(string templatestring, IPackage package)
+		//{
+		//	if (package == null) {
+		//		throw new ArgumentNullException ("package");
+		//	}
+		//	if (string.IsNullOrEmpty (templatestring)) {
+		//		throw new ArgumentException ("templatestring cannot be null or empty.", "templatestring");
+		//	}
+		//	string result = Razor.Parse(templatestring, package);
+		//	return result;
+		//}
 
-		public static void CompileFile(string templatepath, string outpath, IPackage package)
-		{
-			if (package == null) {
-				throw new ArgumentNullException ("package");
-			}
-			if (string.IsNullOrEmpty (templatepath)) {
-				throw new ArgumentException ("templatepath cannot be null or empty.", "templatepath");
-			}
-			if (string.IsNullOrEmpty (outpath)) {
-				throw new ArgumentException ("outpath cannot be null or empty.", "outpath");
-			}
-			string template = IO.File.ReadAllText (templatepath);
-			string result = Razor.Parse(template, package);
-			IO.File.WriteAllText (outpath, result);
-		}
+		//public static void CompileFile(string templatepath, string outpath, IPackage package)
+		//{
+		//	if (package == null) {
+		//		throw new ArgumentNullException ("package");
+		//	}
+		//	if (string.IsNullOrEmpty (templatepath)) {
+		//		throw new ArgumentException ("templatepath cannot be null or empty.", "templatepath");
+		//	}
+		//	if (string.IsNullOrEmpty (outpath)) {
+		//		throw new ArgumentException ("outpath cannot be null or empty.", "outpath");
+		//	}
+		//	string template = IO.File.ReadAllText (templatepath);
+		//	string result = Razor.Parse(template, package);
+		//	IO.File.WriteAllText (outpath, result);
+		//}
 
-        public static void CompileFile(string templatepath, IEnumerable<string> parentpaths, string outpath, IPackage package)
+        //public static void CompileFile(string templatepath, IEnumerable<string> parentpaths, string outpath, IPackage package)
+        //{
+        //    using (TemplateService service = new TemplateService())
+        //    {
+        //        foreach (string path in parentpaths)
+        //        {
+        //            string tpl = IO.File.ReadAllText (path);
+        //            service.Compile(tpl, typeof(object), IO.Path.GetFileNameWithoutExtension(path));
+        //        }
+        //        string tplstring = IO.File.ReadAllText (templatepath);
+        //        string result = service.Parse(tplstring, package, null, null);
+        //        IO.File.WriteAllText (outpath, result);
+        //    }
+        //}
+
+        public static void CompileFile(string templatedir, string viewname, IPackage package, string outpath)
         {
+            IO.DirectoryInfo indir = new IO.DirectoryInfo(templatedir);
+            IEnumerable<IO.FileInfo> allviewfiles = indir.GetFiles("*.cshtml", IO.SearchOption.AllDirectories);
             using (TemplateService service = new TemplateService())
             {
-                foreach (string path in parentpaths)
+                IO.FileInfo viewfile = null;
+                foreach (IO.FileInfo f in allviewfiles)
                 {
-                    string tpl = IO.File.ReadAllText (path);
-                    service.Compile(tpl, typeof(object), IO.Path.GetFileNameWithoutExtension(path));
+                    string fname = IO.Path.GetFileNameWithoutExtension(f.FullName);
+                    if (fname != viewname)
+                    {
+                        string tpl = IO.File.ReadAllText(f.FullName);
+                        service.Compile(tpl, typeof(object), fname);
+                    }
+                    else
+                    {
+                        viewfile = f;
+                    }
                 }
-                string tplstring = IO.File.ReadAllText (templatepath);
+                if (viewfile == null)
+                {
+                    throw new ArgumentException("A view named " + viewname + " could not be found.", "viewname");
+                }
+                string tplstring = IO.File.ReadAllText (viewfile.FullName);
                 string result = service.Parse(tplstring, package, null, null);
                 IO.File.WriteAllText (outpath, result);
             }
-        }
-
-        public static void Compile()
-        {
-
         }
     }
 }
---
+++ Waldi.Tests/Engine/TemplateTests.cs
@@ -0,0 +1,76 @@
+using System;
+using IO = System.IO;
+using NUnit.Framework;
+using Waldi.Engine;
+using Waldi.Packages;
+using Waldi.BclExtensions;
+
+namespace Waldi.Tests
+{
+    public class TemplateTests
+    {
+        [TestFixtureSetUp]
+        public void SetUpFixture()
+        {
+            string[] filepaths = new string[] {
+                IO.Path.Combine("Testdata", "razortemplates", "simplestrongly", "hellopackageStrongly.cshtml"),
+                IO.Path.Combine("Testdata", "razortemplates", "simpleweakly", "hellopackageWeakly.cshtml"),
+                IO.Path.Combine("Testdata", "razortemplates", "simplewithlayout", "hellopackage.cshtml"),
+                IO.Path.Combine("Testdata", "razortemplates", "simplewithlayout", "layout.cshtml")
+            };
+            ItemDeployment.DeployFiles (filepaths, true);
+        }
+
+        [Test]
+        public void CompileSimpleStrongly()
+        {
+            BasicPackage pkg = new BasicPackage("mycoolpackage");
+            string tpldir = IO.Path.Combine("Testdata", "razortemplates", "simplestrongly");
+            string outpath;
+            if (!PathExtensions.GetTempPath(out outpath))
+            {
+                throw new Exception("Could not get temp path.");
+            }
+            outpath = IO.Path.Combine(outpath, "result.html");
+            Template.CompileFile(tpldir, "hellopackageStrongly", pkg, outpath);
+            string viewcontent = IO.File.ReadAllText(outpath);
+            string expectedcontent = "<html>\r\n\t<head>\r\n\t\t<title>Simple Template (strongly typed model)</title>\r\n\t</head>\r\n\t<body>\r\n\t\tHello mycoolpackage!\r\n\t</body>\r\n</html>";
+            Assert.AreEqual(expectedcontent, viewcontent);
+        }
+
+        [Test]
+        public void CompileSimpleWeakly()
+        {
+            BasicPackage pkg = new BasicPackage("mycoolpackage");
+            string tpldir = IO.Path.Combine("Testdata", "razortemplates", "simpleweakly");
+            string outpath;
+            if (!PathExtensions.GetTempPath(out outpath))
+            {
+                throw new Exception("Could not get temp path.");
+            }
+            outpath = IO.Path.Combine(outpath, "result.html");
+            Template.CompileFile(tpldir, "hellopackageWeakly", pkg, outpath);
+            string viewcontent = IO.File.ReadAllText(outpath);
+            string expectedcontent = "<html>\r\n\t<head>\r\n\t\t<title>Simple Template (weakly typed model)</title>\r\n\t</head>\r\n\t<body>\r\n\t\tHello mycoolpackage!\r\n\t</body>\r\n</html>";
+            Assert.AreEqual(expectedcontent, viewcontent);
+        }
+
+        [Test]
+        public void CompileSimpleWithLayout()
+        {
+            BasicPackage pkg = new BasicPackage("mycoolpackage");
+            string tpldir = IO.Path.Combine("Testdata", "razortemplates", "simplewithlayout");
+            string outpath;
+            if (!PathExtensions.GetTempPath(out outpath))
+            {
+                throw new Exception("Could not get temp path.");
+            }
+            outpath = IO.Path.Combine(outpath, "result.html");
+            Template.CompileFile(tpldir, "hellopackage", pkg, outpath);
+            string viewcontent = IO.File.ReadAllText(outpath);
+            string expectedcontent = "<html>\r\n\t<head>\r\n\t\t<title>Simple Template with layout</title>\r\n\t</head>\r\n\t<body>\r\n\t\t\r\nHello mycoolpackage!\r\n\t</body>\r\n</html>";
+            Assert.AreEqual(expectedcontent, viewcontent);
+        }
+    }
+}
+
