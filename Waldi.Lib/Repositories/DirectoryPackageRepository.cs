73c5e470e0182030b16d86531d31161f2fcf35c6
Andreas Fachathaler <faandi@fachi.net>
2013-08-21 22:13:19 +0000

    Fix bug

--- Waldi.Lib/Repositories/DirectoryPackageRepository.cs
+++ Waldi.Lib/Repositories/DirectoryPackageRepository.cs
@@ -69,6 +69,10 @@ namespace Waldi.Repositories
                 return null;
             }
             IPackage newpkg = this.ReadPackageDir(pkgdir);
+            if (newpkg == null)
+            {
+                return null;
+            }
             if (this.packages == null)
             {
                 this.packages = new PackageList();
