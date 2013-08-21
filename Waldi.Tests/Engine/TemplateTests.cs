a495631d7944a0aed5aff7471a7eacee06cee1d0
Andreas Fachathaler <faandi@fachi.net>
2013-08-21 11:27:08 +0200

    Change hardcoded line endings to environment line endings

--- Waldi.Tests/Engine/TemplateTests.cs
+++ Waldi.Tests/Engine/TemplateTests.cs
@@ -34,7 +34,7 @@ namespace Waldi.Tests
             outpath = IO.Path.Combine(outpath, "result.html");
             Template.CompileFile(tpldir, "hellopackageStrongly", pkg, outpath);
             string viewcontent = IO.File.ReadAllText(outpath);
-            string expectedcontent = "<html>\r\n\t<head>\r\n\t\t<title>Simple Template (strongly typed model)</title>\r\n\t</head>\r\n\t<body>\r\n\t\tHello mycoolpackage!\r\n\t</body>\r\n</html>";
+            string expectedcontent = string.Format("<html>{0}\t<head>{0}\t\t<title>Simple Template (strongly typed model)</title>{0}\t</head>{0}\t<body>{0}\t\tHello mycoolpackage!{0}\t</body>{0}</html>", Environment.NewLine);
             Assert.AreEqual(expectedcontent, viewcontent);
         }
 
@@ -51,7 +51,7 @@ namespace Waldi.Tests
             outpath = IO.Path.Combine(outpath, "result.html");
             Template.CompileFile(tpldir, "hellopackageWeakly", pkg, outpath);
             string viewcontent = IO.File.ReadAllText(outpath);
-            string expectedcontent = "<html>\r\n\t<head>\r\n\t\t<title>Simple Template (weakly typed model)</title>\r\n\t</head>\r\n\t<body>\r\n\t\tHello mycoolpackage!\r\n\t</body>\r\n</html>";
+            string expectedcontent = string.Format("<html>{0}\t<head>{0}\t\t<title>Simple Template (weakly typed model)</title>{0}\t</head>{0}\t<body>{0}\t\tHello mycoolpackage!{0}\t</body>{0}</html>", Environment.NewLine);
             Assert.AreEqual(expectedcontent, viewcontent);
         }
 
@@ -68,7 +68,7 @@ namespace Waldi.Tests
             outpath = IO.Path.Combine(outpath, "result.html");
             Template.CompileFile(tpldir, "hellopackage", pkg, outpath);
             string viewcontent = IO.File.ReadAllText(outpath);
-            string expectedcontent = "<html>\r\n\t<head>\r\n\t\t<title>Simple Template with layout</title>\r\n\t</head>\r\n\t<body>\r\n\t\t\r\nHello mycoolpackage!\r\n\t</body>\r\n</html>";
+            string expectedcontent = string.Format("<html>{0}\t<head>{0}\t\t<title>Simple Template with layout</title>{0}\t</head>{0}\t<body>{0}\t\t{0}Hello mycoolpackage!{0}\t</body>{0}</html>", Environment.NewLine);
             Assert.AreEqual(expectedcontent, viewcontent);
         }
     }
